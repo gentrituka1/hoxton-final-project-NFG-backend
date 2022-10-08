33796102993068d479d20fa07c54e5f0ad01ba31
Gentrit Uka <gentrit-uka@live.com>
2022-10-08 16:50:16 +0200

    updated

--- index.ts
+++ index.ts
@@ -1,4 +1,100 @@
-import express from 'express';
-import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
-import 
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+const SECRET = process.env.SECRET!;
+
+function getToken(id: number) {
+  return jwt.sign({ id }, SECRET, { expiresIn: "2h" });
+}
+
+async function getCurrentUser(token: string) {
+  const decodedData = jwt.verify(token, SECRET);
+  const user = await prisma.user.findUnique({
+    where: {
+      // @ts-ignore
+      id: Number(decodedData.id),
+    },
+    include: {
+      posts: true,
+    },
+  });
+  return user;
+}
+
+app.post("/register", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({
+      where: {
+        OR: [
+          {
+            email: req.body.email,
+          },
+          {
+            nickname: req.body.nickname,
+          },
+        ],
+      },
+    });
+    if (users.length > 0) {
+      res.status(400).json({ message: "User already exists" });
+    } else {
+      const user = await prisma.user.create({
+        data: {
+          name: req.body.name,
+          email: req.body.email,
+          nickname: req.body.nickname,
+          password: bcrypt.hashSync(req.body.password, 10),
+        },
+        include: {
+          posts: true,
+          comments: true,
+          _count: { select: { upvotes: true } },
+        },
+      });
+      const token = getToken(user.id);
+      res.send({ user, token });
+    }
+  } catch (error) {
+    // @ts-ignore
+    res.status(451).json({ error: error.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const users = await prisma.user.findMany({
+    where: {
+      OR: [
+        {
+          email: req.body.login,
+        },
+        {
+          nickname: req.body.login,
+        },
+      ],
+    },
+    include: {
+      posts: true,
+      comments: true,
+      _count: { select: { upvotes: true } },
+    },
+  });
+    const user = users[0];
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      const token = getToken(user.id);
+      res.send({ user, token });
+    } else {
+        res.status(400).json({ message: "Invalid credentials" });
+    }
+});
---
+++ prisma/migrations/20221008142747_added_models/migration.sql
@@ -0,0 +1,39 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "image" TEXT,
+    "saved" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Upvote" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "postId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Upvote_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Upvote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "text" TEXT NOT NULL,
+    "postId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
